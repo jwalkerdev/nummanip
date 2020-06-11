5f1b275facdfa33999cb118a64ff6b124e4f5d46
John Walker Jr <john_walkerjr@comcast.com>
2020-06-11 18:22:55 -0500

    Change math.go to use variadic arg

--- calc/math.go
+++ calc/math.go
@@ -12,6 +12,10 @@ func init() {
 }
 
 // Add - returns sum of two integers
-func Add(i int, j int) int {
-	return i + j
+func Add(numbers ...int) int {
+	sum := 0
+	for _, num := range numbers {
+		sum = sum + num
+	}
+	return sum
 }
