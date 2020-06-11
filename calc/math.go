6b8f610d9e3606191b13beea9de5c0f1c8514ed8
John Walker Jr <john_walkerjr@comcast.com>
2020-06-11 18:33:34 -0500

    Change Add() interface

--- calc/math.go
+++ calc/math.go
@@ -1,6 +1,7 @@
 package calc
 
 import "fmt"
+import "errors"
 
 // init - runs when the package is initialized. Can have multiple
 func init() {
@@ -12,10 +13,13 @@ func init() {
 }
 
 // Add - returns sum of two integers
-func Add(numbers ...int) int {
+func Add(numbers ...int) (int, error) {
 	sum := 0
+	if len(numbers) < 2 {
+		return sum, errors.New("provide at least 2 numbers")
+	}
 	for _, num := range numbers {
 		sum = sum + num
 	}
-	return sum
+	return sum, nil
 }
