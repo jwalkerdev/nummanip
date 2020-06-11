532be4fe7b030cb5eb509182fa9ab99871f0e35f
John Walker Jr <john_walkerjr@comcast.com>
2020-06-11 15:50:09 -0500

    Update math.go

--- calc/math.go
+++ calc/math.go
@@ -1,5 +1,16 @@
 package calc
 
+import "fmt"
+
+// init - runs when the package is initialized. Can have multiple
+func init() {
+	fmt.Println("calc/math.go ==> init() [1]")
+}
+
+func init() {
+	fmt.Println("calc/math.go ==> init() [2]")
+}
+
 // Add - returns sum of two integers
 func Add(i int, j int) int {
 	return i + j
